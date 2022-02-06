033d79fd0ea2198d380022b706f53f6b0e937196
Julian Bolanos <julian.camilob24@gmail.com>
2022-02-06 16:08:26 -0500

    Register class added to ViewModel

---
+++ ViewModels/Register.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApp.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string Birthday { get; set; }
+    }
+}
